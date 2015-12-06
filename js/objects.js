2fbb9ef3771fcee80a558f2ae8ba40f7f7aec4c6
vytalman <ryanmesa@gmail.com>
2015-12-06 02:18:47 -0500

    create objects.js, rename images

---
+++ js/objects.js
@@ -0,0 +1,34 @@
+var question1 = {
+	image: "images/image1.png",
+	question: "Which video game does this character appear in?",
+	options: ["Legend of Zelda: Ocarina of Time", "Final Fantasy", "Ghost ’n Goblins", "Legend of Zelda: A Link to the Past"],
+	answer: "Legend of Zelda: A Link to the Past"
+};
+
+var question2 = {
+	image: "images/image2.png",
+	question: "What’s the name of the final boss in the game this character appears in?",
+	options: ["Andross", "Red Falcon", "Mother Brain", "Gannon"],
+	answer: "Red Falcon"
+};
+
+var question3 = {
+	image: "images/image3.png",
+	question: "What is his name?",
+	options: ["Ryu", "Ken", "Shinobi", "Snake"],
+	answer: "Ryu"
+};
+
+var question4 = {
+	image: "images/image4.png",
+	question: "What is the name of the character who is kidnapped in the game this battletoad belongs to, kicking off the adventure?",
+	options: ["Zitz", "Rash", "Blackhead", "Pimple"],
+	answer: "Pimple"
+}:
+
+var question5 = {
+	image: "images/image5.png",
+	question: "Which game does this warrior belong to?",
+	options: ["Metroid", "Rygar", "Blaster Master", "Galaga"],
+	answer: "Rygar"
+};
