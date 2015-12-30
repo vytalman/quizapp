6200517000aab8a59bc53c0fbe634daa598f711d
vytalman <ryanmesa@gmail.com>
2015-12-30 00:08:12 -0500

    add function to start button

--- js/app.js
+++ js/app.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 	function changeTvImage(tvImage) {
 		document.getElementById("tv").src = tvImage;
 	}
+	
+	function startGame() {
+		$('.hide-after-start').fadeOut('slow');
+		$('.start-hidden').delay(900).fadeIn('slow');
+	}
+	
+	$("#nesStartButton").click(function() {
+		startGame();
+	});
 });
